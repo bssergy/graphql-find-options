4a3424290ad7f1b32995d7f4283a2dec0575f3ce
Sergiy Brao <bssergy@gmail.com>
2020-06-15 07:28:28 +0300

    fixed querying nested

--- src/index.ts
+++ src/index.ts
@@ -219,5 +219,5 @@ export async function findAllWithNested<P extends Model<P>, T extends Model<T>>(
   const as = Object.values(parentEntity.associations).find(x => x.target === entity).as;
   return parentEntity
     .findAll(await getFindOptionsForNested(parentEntityIds, parentEntity, entity, args, fieldNode))
-    .then(result => result.map(x => x[as]));
+    .then(result => parentEntityIds.map(x => result.find(y => y.id === x)));
 }
